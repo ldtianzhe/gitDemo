405ebd360646290458fd0a40065445adf1fe75ac
tianzhe <ldtianzhe@163.com>
2021-06-29 16:50:49 +0800

    first commit

---
+++ src/main/java/com/example/gitdemo/test/Test.java
@@ -0,0 +1,13 @@
+package com.example.gitdemo.test;
+
+/**
+ * @author ldtianzhe
+ * @description
+ * @date 2021/6/29
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String str = "first";
+    }
+}
