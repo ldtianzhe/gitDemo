2426d8ffa34ffa1c9c3b29af148667d895beb9eb
tianzhe <ldtianzhe@163.com>
2022-01-24 14:45:12 +0800

    second commit

--- src/main/java/com/example/gitdemo/test/Test.java
+++ src/main/java/com/example/gitdemo/test/Test.java
@@ -1,5 +1,7 @@
 package com.example.gitdemo.test;
 
+import java.util.Random;
+
 /**
  * @author ldtianzhe
  * @description
@@ -8,6 +10,19 @@ package com.example.gitdemo.test;
 public class Test {
 
     public static void main(String[] args) {
-        String str = "first";
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("<<<<<<<========>>>>>>>>");
+            // String str = new Random().nextInt() % 2 == 0 ? null : "not null";
+
+            // System.out.println(str);
+            // assert str != null;
+
+            Boolean bo = 1 > 2;
+            assert bo;
+            System.out.println(".....");
+            System.out.println(">>>>>>>=========<<<<<<");
+        }
+
     }
 }
